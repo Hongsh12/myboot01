c428b83998b26ece56d62200f72346a8fb532434
hong <hss0935@naver.com>
2019-11-13 13:00:57 +0900

    myboot01

--- myboot01/src/main/java/com/myboot01/DemoController.java
+++ myboot01/src/main/java/com/myboot01/DemoController.java
@@ -1,6 +1,7 @@
 package com.myboot01;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,9 +12,18 @@ public class DemoController {
 	@RequestMapping("/") // 모든 요청을 처리
 	public String home() {
 		
-		System.out.println("hello");
+		System.out.println("hello"); 
 		
 		return "hello"; // 브라우저로 출력
+	}
+	
+	@RequestMapping("/hello.do")
+	public String hello(Model model) {
+		
+		System.out.println("안녕하세요");
+		model.addAttribute("message","hello.html 입니다!");
+		
+		return "hello";
 	}
 	
 
